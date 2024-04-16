234cb69e3a5274769e407f908b4f0b73514275c9
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-04-16 20:13:24 +0330

    starting ecommerce project. react, typescript and tailwind installed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-pink-700 text-7xl font-bold'>Hello world!</h1>
+      <h2 className='text-pink-500 text-4xl'>Start of an ecommerce website</h2>
+    </div>
+      
+  )
+}
+
+export default App
