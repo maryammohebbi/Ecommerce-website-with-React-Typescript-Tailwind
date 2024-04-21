81239b164060987bc47a2d7bcb2e56b6f04dd620
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-04-21 23:32:56 +0330

    minor changes

--- src/features/home/Slider.tsx
+++ src/features/home/Slider.tsx
@@ -1,20 +1,23 @@
 // import React from 'react';
-import SwiperCore from 'swiper/core';
-import { Navigation } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import SwiperCore from 'swiper/core';
+// import { Navigation } from 'swiper/modules'
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
-SwiperCore.use([Navigation]);
+// SwiperCore.use([Navigation]);
 
 type Props = {};
 
 function Slider({}: Props) {
   return (
     <div>
-      <Swiper navigation={true}>
+      {/* <Swiper navigation={true}>
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
-      </Swiper>
+      </Swiper> */}
+      <div className="w-full h-[calc(100vh-3rem)] bg-green-300">
+
+      </div>
     </div>
   );
 }
