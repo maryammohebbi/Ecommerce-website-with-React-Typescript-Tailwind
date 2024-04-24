a64d6c2ff695adbf8c1598e2b520d05620136073
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-04-24 16:39:36 +0330

    tanstack react query installed

---
+++ src/features/categories/Category.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Category({}: Props) {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
---
+++ src/features/categories/CategoryContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function CategoryContainer({}: Props) {
+  return (
+    <div>CategoryContainer</div>
+  )
+}
+
+export default CategoryContainer
