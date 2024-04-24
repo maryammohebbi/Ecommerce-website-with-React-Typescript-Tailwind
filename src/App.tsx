36d2740f4a167ec74a9e82e4437025d4d13b091a
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-04-24 16:42:26 +0330

    react query config done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./features/home/Home"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
-    </>
+    </QueryClientProvider>
       
   )
 }
