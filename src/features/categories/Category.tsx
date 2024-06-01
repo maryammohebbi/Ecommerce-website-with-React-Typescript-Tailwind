11af0943b55006eae6c8a6f3a441a52d12484fe3
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-06-01 20:01:17 +0330

    Category component developed and got render in CategoryContainer, and react router dom installed

--- src/features/categories/Category.tsx
+++ src/features/categories/Category.tsx
@@ -1,15 +1,57 @@
 // import React from 'react'
+import { GiJewelCrown } from "react-icons/gi"
+import { FcElectronics } from "react-icons/fc"
+import { GiClothes } from "react-icons/gi"
+import { PiDressDuotone } from "react-icons/pi"
 
-type Props = {}
+const icons = [
+  {
+    title: "electronics",
+    icon: <FcElectronics className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
+  },
+  {
+    title: "jewelery",
+    icon: <GiJewelCrown className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
+  },
+  {
+    title: "men's clothing",
+    icon: <GiClothes className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
+  },
+  {
+    title: "women's clothing",
+    icon: <PiDressDuotone className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
+  },
+]
 
-function Category({}: Props) {
+interface Categories {
+  categories: string[]
+}
+
+function Category({categories}: Categories) {
+
+  const findAndRenderIcon = (categoryTitle: string) => {
+    const matchingIcon = icons.find(
+      (icon) => icon.title.toLowerCase() === categoryTitle.toLowerCase()
+    )
+  
+    if (matchingIcon) {
+      return matchingIcon.icon;
+    }
+  }
+
+
   return (
-    <div>
-      <div className="bg-pink-100 w-[4rem] h-[4rem] rounded-full">
-        <img src="" alt="" />
-        <span></span>
+    <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-2 md:gap-x-40 md:h-screen ">
+        {categories.map((category) => (
+          <div
+            key={category}
+            className="w-[8rem] h-[8rem] md:w-[15rem] md:h-[15rem] relative flex flex-col items-center"
+          >
+            {findAndRenderIcon(category)}
+            <span className="">{category}</span>
+          </div>
+        ))}
       </div>
-    </div>
   )
 }
 
--- src/features/categories/CategoryContainer.tsx
+++ src/features/categories/CategoryContainer.tsx
@@ -1,62 +1,19 @@
 // import React from 'react';
 
 import useCategories from "./useCategories"
-import { GiJewelCrown } from "react-icons/gi"
-import { FcElectronics } from "react-icons/fc"
-import { GiClothes } from "react-icons/gi"
-import { PiDressDuotone } from "react-icons/pi"
+import Category from "./Category"
 
-const icons = [
-  {
-    title: "electronics",
-    icon: <FcElectronics className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
-  },
-  {
-    title: "jewelery",
-    icon: <GiJewelCrown className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
-  },
-  {
-    title: "men's clothing",
-    icon: <GiClothes className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
-  },
-  {
-    title: "women's clothing",
-    icon: <PiDressDuotone className="w-[80%] h-[80%] p-2 bg-secondaryBg rounded-full"/>,
-  },
-]
 
-// interface Props {}
 const CategoryContainer = () => {
   const { isCategoriesLoading, categories } = useCategories()
 
   if (isCategoriesLoading) return <p>Loading...</p>
   if (!categories) return <p>No categories available</p>
 
-
-  const findAndRenderIcon = (categoryTitle: string) => {
-    const matchingIcon = icons.find(
-      (icon) => icon.title.toLowerCase() === categoryTitle.toLowerCase()
-    )
-  
-    if (matchingIcon) {
-      return matchingIcon.icon;
-    }
-  }
-
   return (
     <div className="container p-5 flex flex-col items-center ">
       <h3>Product Categories</h3>
-      <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 gap-2 md:gap-x-40 md:h-screen ">
-        {categories.map((category) => (
-          <div
-            key={category}
-            className="w-[8rem] h-[8rem] md:w-[15rem] md:h-[15rem] relative flex flex-col items-center"
-          >
-            {findAndRenderIcon(category)}
-            <span className="">{category}</span>
-          </div>
-        ))}
-      </div>
+      <Category categories={categories}/>
     </div>
   );
 }
