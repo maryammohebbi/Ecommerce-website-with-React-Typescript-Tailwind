48d507f80adf5d1ea5606c7285fcdf08523cf591
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-07-01 23:13:52 +0330

    hook form config for login component. still uncompleted and needs to fix the bugs

--- src/authentication/Login.tsx
+++ src/authentication/Login.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import InputField from '../features/ui/InputField'
+import { useForm } from 'react-hook-form'
+
+type Inputs = {
+  name: string
+  password: string
+}
 
 const Login: React.FC = ({}) => {
+  const { register, handleSubmit } = useForm<Inputs>()
+
+  const loginReqHandler = () => {}
+
   return (
     <div className="">
-      <form className="flex flex-col gap-y-8">
-        <InputField type="text" placeholder="YOUR EMAIL OR USERNAME" />
-        <InputField type="password" placeholder="PASSWORD" />
+      <form
+        onSubmit={handleSubmit(loginReqHandler)}
+        className="flex flex-col gap-y-8"
+      >
+        <InputField
+          name="userName"
+          register={register}
+          required
+          type="text"
+          placeholder="YOUR EMAIL OR USERNAME"
+        />
+        <InputField
+          name="password"
+          register={register}
+          required
+          type="password"
+          placeholder="PASSWORD"
+        />
         <button className="p-2 bg-green-300 rounded-lg text-lg font-semibold">
           Login
         </button>
--- src/features/ui/InputField.tsx
+++ src/features/ui/InputField.tsx
@@ -3,9 +3,14 @@ interface InutProps {
   type: string
   placeholder: string
 }
-const InputField: React.FC<InutProps> = ({ type, placeholder }) => {
+const InputField: React.FC<InutProps> = ({ type, placeholder, register, name }) => {
   return (
-    <input className="p-2 rounded-lg" type={type} placeholder={placeholder} />
+    <input
+      className="p-2 rounded-lg"
+      {...register(name)}
+      type={type}
+      placeholder={placeholder}
+    />
   )
 }
 
