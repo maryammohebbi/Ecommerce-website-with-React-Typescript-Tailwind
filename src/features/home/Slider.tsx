7bf96eabe940362661ee4a3a5bfc81c7c3c2577a
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-08-12 00:59:03 +0330

    landing slider added. needs styles

--- src/features/home/Slider.tsx
+++ src/features/home/Slider.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { Navigation } from 'swiper/modules'
 
 type Props = {}
 
 const Slider: React.FC<Props> = ({}) => {
   return (
     <div>
-      <div className="w-full h-[calc(100vh-3rem)] bg-green-300">
-        <Link to="/products">
-          <button className="p-2 bg-pink-200">Go to the shop</button>
-        </Link>
+      <div className="w-full h-[calc(100vh-3rem)]">
+        <Swiper
+          modules={[Navigation]}
+          slidesPerView={1}
+          navigation
+          className="w-full h-full relative"
+        >
+          <SwiperSlide>Hi</SwiperSlide>
+          <SwiperSlide>Bye</SwiperSlide>
+          <Link to="/products">
+            <button className="p-2 bg-pink-200 z-50 absolute top-0 right-0">
+              Go to the shop
+            </button>
+          </Link>
+        </Swiper>
       </div>
     </div>
   )
