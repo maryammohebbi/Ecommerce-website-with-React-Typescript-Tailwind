9a466900a1616d90301dc67b96fa0f7c2b70b35f
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-12-06 18:00:06 +0330

    minor changes

--- src/features/ui/MobileMenu.tsx
+++ src/features/ui/MobileMenu.tsx
@@ -13,7 +13,7 @@ import Search from './Search'
 
 const MobileMenu: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const user = useUser()
+  const { user } = useUser()
   const { setCartNumber } = useCartNumber()
   const { carts } = useCarts()
 
